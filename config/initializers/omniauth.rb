1580d381bf8dc4f3452a539701d3d1e857c53c70
Achal Channarasappa <achannar@andrew.cmu.edu>
2015-11-16 18:12:07 -0500

    fb auth works

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,2 +1,5 @@
-provider :facebook, ENV['facebook_key'], ENV['facebook_secret'],
-scope: 'public_profile', info_fields: 'id,name,link'
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '417122608498927', '64e77b7989f7af40d7ab05d0014eac90'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114231259) do
+ActiveRecord::Schema.define(version: 20151116210842) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -58,8 +58,13 @@ ActiveRecord::Schema.define(version: 20151114231259) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "oauth_token"
+    t.string   "name"
+    t.datetime "oauth_expires_at"
   end
 
 end
