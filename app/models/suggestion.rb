243daa6a9b61130c976e916530dc42b686ff3a27
Swathi Anand <slanand@andrew.cmu.edu>
2015-12-02 00:37:52 -0500

    made name optional and removed name field from form

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -6,7 +6,7 @@ class Suggestion < ActiveRecord::Base
 	has_many :notifications
 
 	# validations
-	validates_presence_of :name, :description
+	validates_presence_of :description
 	EXPERIENCE_TYPES = [['positive', 'positive'], ['okay', 'okay'], ['negative', 'negative']]
 	validates :experience_type, presence: true, inclusion: { in: EXPERIENCE_TYPES.map{|a,b| b}, message: "is not a valid experience type"}
 	CATEGORIES_LIST = [['Restaurant/Bar', 'restaurant/bar'], ['Attraction', 'attraction'], ['Event/Festival', 'event/festival'], ['Nightlife', 'nightlife'], ['Shopping', 'shopping'], ['Museum', 'museum'], ['Other', 'other']]
